12b0bfc387f413df1186ceb04489077705cfb6d8
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2023-12-31 00:30:13 +0300

    Updated othersds_init.py to 0.5b

--- gee/modules/othersds_init.py
+++ gee/modules/othersds_init.py
@@ -1,18 +1,26 @@
 import ee
 
-def others_initialization(m, date_one, date_two):
-    firms = ee.ImageCollection("FIRMS").filterDate(str(date_one.value), str(date_two.value))
-    vis_firms = {
-        min: 325.0,
-        max: 400.0,
-    }
+class OthersInitialization:
+    def __init__(self, m, date_one, date_two):
+        self.m = m
+        self.date_one = date_one
+        self.date_two = date_two
 
-    alos_td = ee.Image("CSP/ERGo/1_0/Global/ALOS_topoDiversity")
-    alos_topodiv = alos_td.select('constant')
-    vis_alos_td = {
-        min: 0.0,
-        max: 1.0,
-    }
+    def firms_init(self):
+        firms = ee.ImageCollection("FIRMS").filterDate(str(self.date_one.value), str(self.date_two.value))
+        vis_firms = {
+            min: 325.0,
+            max: 400.0,
+        }
 
-    m.addLayer(firms, vis_firms, "NASA FIRMS")
-    m.addLayer(alos_topodiv, vis_alos_td, "ALOS TD")
+        self.m.addLayer(firms, vis_firms, "NASA FIRMS")
+
+    def alostd_init(self):
+        alos_td = ee.Image("CSP/ERGo/1_0/Global/ALOS_topoDiversity")
+        alos_topodiv = alos_td.select('constant')
+        vis_alos_td = {
+            min: 0.0,
+            max: 1.0,
+        }
+
+        self.m.addLayer(alos_topodiv, vis_alos_td, "ALOS TD")
